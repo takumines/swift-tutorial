001b9ffe3aff12dc5fc67d4f86a75dda409bd19b
takumi_kurogi <takumines1@gmail.com>
2023-04-01 11:49:34 +0900

    Initial Commit

---
+++ swift-tutorial/swift_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorialApp.swift
+//  swift-tutorial
+//
+//  Created by takumi kurogi on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
